39cc2b7b4d3472adc670cfc9ed178613f471b88e
BBAMO <gao0708@naver.com>
2025-02-04 15:49:03 +0900

    이동

--- Assets/2. Scripts/RubyController.cs
+++ Assets/2. Scripts/RubyController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    const float xs = 4.0f;
+    const float ys = 4.0f;
     void Start()
     {
         
     }
 
+
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // GetAxisRaw를 사용하면 -1, 1 값이 넘어온다.
+        //float vertical = Input.GetAxisRaw("Vertical");
+        //Debug.Log($"H:{horizontal}");
+        //Debug.Log($"V: {vertical}");
+        
         Vector2 position = transform.position;
-        position.x += 0.1f;
+        position.x += xs * horizontal * Time.deltaTime;
+        position.y += ys * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
